baab15c4b7c53ba978d88ec01c600354e41193ae
Dedy Yugo Purwanto <dedy.yugo.purwanto@gmail.com>
2017-05-26 13:44:12 +0700

    Report index view

---
+++ resources/views/file/report/index.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="col-md-12">
+	<h4 class="tittle-w3layouts">{{ $title }}</h4>
+
+	<div class="table-responsive">
+		<table class="table table-bordered">
+			<thead>
+				<tr>
+					<th>File Label</th>
+					<th>Reporter</th>
+					<th>Reported at</th>
+					<th>Message</th>
+					<th>Actions</th>
+				</tr>
+			</thead>
+
+			<tbody>
+				@foreach ($reports as $report)
+					<tr>
+						<td>
+							@if (! empty($report->user))
+								<a href="#">
+									{{ $report->user->name }} ({{ $report->user->email }})
+									<i class="fa fa-external-link fa-fw"></i>
+								</a>
+							@else
+								{{ $report->name }} ({{ $report->email }})
+							@endif
+						</td>
+						<td>
+							<a href="{{ route('file.view', $report->file->uuid) }}">
+								{{ $report->file->label }}
+							</a>
+						</td>
+						<td>{{ $report->message }}</td>
+						<td>{{ $report->created_at->format('Y-m-d H:i') }}</td>
+						<td>
+							<a href="#" class="btn btn-xs btn-danger" title="Delete file forever">
+								<i class="fa fa-trash fa-fw"></i>
+							</a>
+							<a href="#" class="btn btn-xs btn-success" title="This file was fine">
+								<i class="fa fa-check fa-fw"></i>
+							</a>
+						</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+
+		{{ $reports->links() }}
+	</div>
+</div>
+@endsection
